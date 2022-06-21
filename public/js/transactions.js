fec0a4146c80b55d1eba53772426102307c87fe6
Andreas Jakob <aj.jakob93@gmail.com>
2022-06-20 23:45:22 -0300

    projeto nikel corrigido(1)

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,93 +1,80 @@
 const myModal= new bootstrap.Modal("#transaction-modal");
-let logged= sessionStorage.getItem("logged");
+let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
-let cashIn=[];
-let cashOut=[];
-
-let data= {
-    transactions:[]
-};
+let data = {transactions:[]};
 
 document.getElementById("button-logout").addEventListener("click", logout);
 
+//add lançamento 
+document.getElementById("transation-form").addEventListener("submit",function(e){
+    e.preventDefault();
 
-function checkLogged()
-{
-    if(session)
-    {
+    const value= parseFloat(document.getAnimations("value-input").value);
+    const description =document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({value: value, type: type, descripton: description, date: date});
+    
+   saveData(data);
+    e.target.reset();
+    myModal.hide();
+    getTransations();
+   
+   alert("lançamento add");
+});
+
+function logout(){
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href="index.html";
+}
+
+
+checkLogged();
+
+function checkLogged(){
+    if(session){
         sessionStorage.setItem("logged", session);
         logged =session;
     }
 
-    if(!logged)
-    {
-        window.location.href = "index.html";
+    if(!logged){
+        window.location.href ="home.html";
         return;
     }
-
-    const dataUser=localStorage.getItem(logged);
-    if(dataUser)
-    {
+    const dadaUser= localStorage.getItem(logged);
+    if(dataUser){
         data = JSON.parse(dataUser);
     }
-   getTransactions();
+    getTransations();
 }
 
-
-function logout(){
-    sessionStorage.removeItem("logged");
-    localStorage.removeItem("sesseion");
-    
-    window.location.href="index.html";
-    
-}
-
-//add lançamento
-
-document.getElementById("transaction-form").addEventListener("submit", function(e){
-    e.preventDefault();
-    const value= parseFloat(document.getElementById("value-input").value);
-    const description = document.getElementById("description-input").value;
-    const date = document.getElementById("date-input").value;
-    const type= document.querySelector('input[name="type-input]:checked').value;    
-
-    data.transactions.unshift({
-        value: value, type: type, description: description, date: date
-    });
-});
-saveData(data);
-e.target.myModal();
-myModal.hide();
-getTransactions();
-checkLogged();
-
-function getTransactions(){
+function getTransations(){
     const transactions =data.transactions;
-    let transactionsHtml=``;
+    let transactionsHtml = ``;
 
     if(transactions.length){
         transactions.forEach((item)=> {
             let type ="entrada";
-
             if(item.type==="2"){
-                type ="saída";                
+                type="saída";
             }
-            transactionsHtml+= `
+            transactionsHtml +=`
             <tr>
-                <th scope="row">${item.date}</th>
-                <td>${item.value.tofixed(2)}</td>
-                <td>${type}</td>
-                <td>${item.description}</td>
-          </tr>`
-
+            <th scope="row"> ${item.date}</th>
+            <td>${item.value.toFixed(2)} </th>
+            <td>${item.type} </th>
+            <td>${item.description} </th>
+            </tr>
+            `
         });
+        document.getElementById("transactions-list").innerHTML=transactions;
+
     }
-document.getElementById("transactions-list").innerHTML=transactionsHtml;
 }
 
 function saveData(data){
     localStorage.setItem(data.login, JSON.stringify(data));
 }
-
-
-alert("lançamento add");
